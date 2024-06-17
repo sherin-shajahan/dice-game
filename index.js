16547d7723024776923f8571f857ba830dc28258
sherin-shajahan <sherinshajahan01@gmail.com>
2024-06-18 00:44:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+var randomNumber1 = Math.floor(Math.random() * 6)+1;
+ var randomDice1 = "images/dice" + randomNumber1 + ".png";
+ var image1= document.querySelectorAll("img")[0];
+ image1.setAttribute("src",randomDice1);
+
+
+ var randomNumber2 = Math.floor(Math.random() * 6)+1;
+ var randomDice2 = "images/dice" + randomNumber2 + ".png";
+ var image2 = document.querySelectorAll("img")[1];
+ image2.setAttribute("src",randomDice2);
+
+  
+ if (randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+}
+
+
+if (randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+}
+
+
+if(randomNumber1===randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw";
+}
+
